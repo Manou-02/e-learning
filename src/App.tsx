70828ca6958f24dd4a717d329cdfdf65d2488870
manou <mrakotoarivelo97@gmail.com>
2024-03-03 13:48:58 +0300

    Add timepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { DatePicker } from "antd";
+import { DatePicker, TimePicker } from "antd";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <DatePicker />
+      <div className="flex flex-col gap-[20px]">
+        <DatePicker />
+        <TimePicker />
+      </div>
     </>
   );
 }
